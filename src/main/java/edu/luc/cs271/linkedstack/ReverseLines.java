c71407c4b2be7f464ae1d3df56ceefc71b29e255
Linda Ho <linda.oanh.ho@gmail.com>
2017-10-15 20:45:45 -0500

    Complete ReverseLines class

--- src/main/java/edu/luc/cs271/linkedstack/ReverseLines.java
+++ src/main/java/edu/luc/cs271/linkedstack/ReverseLines.java
@@ -1,14 +1,38 @@
 package edu.luc.cs271.linkedstack;
 
 import java.util.Scanner;
+import java.util.*;
 
 public class ReverseLines {
 
-  public static void main(String[] args) {
-    // TODO read successive input lines until EOF, then print out in reverse order
+    private static Deque<String> inputStack = new ArrayDeque<>();;
 
-    final Scanner input = new Scanner(System.in);
-    String line;
-    while ((line = input.nextLine()) != null) {}
-  }
+    public static void main(String[] args) {
+
+        System.out.println("Enter multiple lines. Press enter twice to reverse the lines!");
+
+        final Scanner input = new Scanner(System.in);
+        String line;
+
+        while (input.hasNextLine()) {
+            line = input.nextLine();
+            if (line.equals("")) {
+                break;
+            } else {
+                fillStack(line);
+            }
+        }
+        printReverse(inputStack);
+    }
+
+    public static void fillStack(String inputString) {
+        inputStack.push(inputString);
+    }
+
+    public static void printReverse(Deque<String> lineStack ){
+        while(!lineStack.isEmpty()) {
+            System.out.println(lineStack.pop());
+        }
+    }
+
 }
